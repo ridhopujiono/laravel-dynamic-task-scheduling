9de82fa7789e1f4782a2a5cd1705190745aa78d4
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-04-06 18:31:19 +0700

    refactor(console): Dynamically schedule tasks from database

--- routes/console.php
+++ routes/console.php
@@ -2,25 +2,46 @@
 
 use Illuminate\Support\Facades\Schedule;
 use App\Models\Schedule as TaskSchedule;
+use Illuminate\Console\Scheduling\Schedule as SchedulingSchedule;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Log;
 
-$taskSchedules = TaskSchedule::where('active', true)->get();
+$days = [
+    'sundays' => SchedulingSchedule::SUNDAY,
+    'mondays' => SchedulingSchedule::MONDAY,
+    'tuesdays' => SchedulingSchedule::TUESDAY,
+    'wednesdays' => SchedulingSchedule::WEDNESDAY,
+    'thursdays' => SchedulingSchedule::THURSDAY,
+    'fridays' => SchedulingSchedule::FRIDAY,
+    'saturdays' => SchedulingSchedule::SATURDAY,
+];
 
-foreach ($taskSchedules as $taskSchedule) {
-    $params = $taskSchedule->params ? explode(",", $taskSchedule->params) : [];
-    $days = $taskSchedule->days ? explode(",", $taskSchedule->days) : [];
 
-    $event = $taskSchedule->command
-        ? Schedule::command($taskSchedule->command)->{$taskSchedule->frequency}(...$params)
-        : Schedule::call(function () use ($taskSchedule) {
-            Log::info("{$taskSchedule->name} run at " . now());
-        })->{$taskSchedule->frequency}(...$params);
+if (Schema::hasTable('schedules')) {
+    $taskSchedules = TaskSchedule::where('active', true)->get();
 
-    foreach ($days as $dayMethod) {
-        if (method_exists($event, $dayMethod)) {
-            $event->{$dayMethod}();
+    foreach ($taskSchedules as $taskSchedule) {
+
+        $event = Schedule::command($taskSchedule->command);
+
+        if ($taskSchedule->frequency) {
+            $event->{$taskSchedule->frequency}();
         }
+
+
+        if ($taskSchedule->days) {
+            $convertedDays = [];
+            foreach ($taskSchedule->days as $day) {
+                $convertedDays[] = $days[$day];
+            }
+            $event->days($convertedDays);
+        }
+
+        if ($taskSchedule->time) {
+            $event->at($taskSchedule->time);
+        }
+
+        $event->timezone('Asia/Jakarta');
+        $event->name($taskSchedule->name);
     }
-    $event->timezone('Asia/Jakarta');
-    $event->name($taskSchedule->name);
 }
